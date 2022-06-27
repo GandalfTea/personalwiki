4919ca5c1aab5e6d2e5fb22ecf788febfb00d893
Octavian Rusu <octavian.concept@gmail.com>
2022-06-27 12:02:12 +0100

    Almost done with correct cell selection

--- prototype/0.1/src/App.jsx
+++ prototype/0.1/src/App.jsx
@@ -19,11 +19,66 @@ const PageHeader = (props) => {
 class File extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { cells: 1 }
+
+		const INITIAL_CELLS = 2;
+
+		this.state = { cells: INITIAL_CELLS, selected: false }
+
+		// store all the cell's data for dump to disk
+		this.ledger = {};
+		for( var i=0; i < INITIAL_CELLS; i++) this.ledger[i] = "";
+		this.update_ledger = this.update_ledger.bind(this);
+
+		// Select and Modify Cells
+		this.selected = {};
+		for( var i=0; i < INITIAL_CELLS; i++) this.selected[i] = false;
+		this.alert_selected = this.alert_selected.bind(this);
 	}
+
+
+	// Watch for UNSELECT if any cell is selected.
+	watch_unselect() {
+
+		// Unselect IF: the user clicks on anything other than an unselected cell 
+		var selected = this.selected;
+		var b_set_selected_false = false;
+		window.onclick = function(e) {
+			const name = e.target.className.split(' ');
+			if(name[0] == 'cell') {
+				if( selected[ e.target.dataset.id] ) {
+					// Deselect all cells
+					for( var i=0; i < Object.keys(selected).length; i++) {
+						console.log("ACTION : DESELECTING ALL")
+						b_set_selected_false = true;
+						selected[toString(i)] = false;
+					}
+				}
+				selected[ toString(e.target.dataset.id) ] = true;
+			}
+		}
+		if( b_set_selected_false) this.setState({ selected: false });
+		this.selected = selected;
+		console.log(this.selected);
+	}
+	
+
+	// FUNCTIONS PASSED DOWN TO CELL CHILD COMPONENTS
+
+	update_ledger( idx, data ) {
+		this.ledger[idx] = data;
+	}
+
+	alert_selected(idx) {
+		this.setState({ selected: true });
+		console.log("CHANGING STATE TO TRUE FOR:", idx);
+		this.selected[idx] = true;
+	}
+
+	// Helpers
 
 	add_cell() { 
 		this.setState({ cells: this.state.cells + 1 });
+		this.selected [this.state.cells-1 ] = false;
 		this.save();
 	}
 
@@ -31,7 +86,6 @@ class File extends React.Component {
 	// Write to disk
 	save() {
 
-		var cells = document.getElementsByClassName('cell');
 		var date = new Date();
 		var bits = {
 			"cells": [],
@@ -39,35 +93,39 @@ class File extends React.Component {
 							+ '@' + date.getHours() + ':' + date.getMinutes()
 		}
 
-		for( var i = 0; i < cells.length; i++ ) {
+		for( var i = 0; i < Object.keys(this.ledger).length; i++ ) {
 			var cell = {
-				data: cells[i].innerText,
+				data: this.ledger[i],
 				metadata: {
-					type: "Markdown"
+					type: "markdown"
 				}
 			}
-
 			bits['cells'][i] = cell;
 		}
-		var json = JSON.stringify(bits);
 
+		var json = JSON.stringify(bits);
+		//console.log(bits);
 	}
 
 	render() {
-
 		var cells = [];
 		for( let i=0; i<this.state.cells; i++) {
-			cells.push( <Cell key={i}/> );
+			cells.push( <Cell key={i} id={i} 
+												update_callback={this.update_ledger} 
+												alert_selected={this.alert_selected} 
+											  selected={ (this.selected[i]) ? "cell-selected " : "" }/> );
 		}
 
 		return(
 			<div className="page">
+				{ (this.state.selected) ? this.watch_unselect() : null }
+				{ (this.state.selected) ? console.log ("WATCHING FOR CLICKS") : null }
 				<PageHeader address="Algebra / Vectors / Vector Arithmatic" title="Vector Arithmetic" />
+				<p class="last-edit">Last Update: 2 March 2022</p>
 				{cells}
 				<div className="add-cell-button" onClick={ () => this.add_cell()} > 
 					<img src="" alt="" />
 				</div>
-				<p class="last-edit">Last Update: 2 March 2022</p>
 			</div>
 		)
 	}
--- prototype/0.1/src/Cell.jsx
+++ prototype/0.1/src/Cell.jsx
@@ -10,8 +10,14 @@ import ReactMarkdown from 'react-markdown';
 class Cell extends React.Component {
 	constructor(props) {
 		super(props);
+		this.id = this.props.id;
 		this.state = { focused: false, data: "" };
 		this.cell_text = React.createRef();
+		this._data = "";
+	}
+
+	componentDidUpdate() {
+		if(this.state.focused) this.cell_text.current.focus();
 	}
 
 	render() {
@@ -20,19 +26,31 @@ class Cell extends React.Component {
 			// Cell is focused.
 			// On blur, copy the text to state to render
 			return(
-				<div className='cell cell-selected' contentEditable tabIndex='0' ref={this.cell_text} 
-							onBlur={ () => {
+				<div className={ (this.state.data=="") ? 'cell cell-selected cell-empty' : 'cell cell-selected'} 
+						 contentEditable tabIndex='0' ref={this.cell_text} 
+						 data-id={ this.id }
+						 onBlur={ () => {
 										this.setState({focused: false})
 
 										// TODO: This does not store empty lines.
 										this.setState({ data: this.cell_text.current.innerText});
+										this._data = JSON.stringify(this.cell_text.current.innerText);
 										this.cell_text.current.innerText = "";
+
+										// Update ledger on File parent class
+										this.props.update_callback(this.id, this._data);
 							}}>
 				{this.state.data}</div>
 			)
 		} else {
 			return(
-				<div className="cell" onClick={ () => this.setState({focused: true})}> 
+				<div className={ (this.state.selected) ? (this.state.data=="") ? 'cell ' + this.props.selected + ' cell-empty'
+																																			 : 'cell ' + this.props.selected 
+																							 : (this.state.data=="") ? 'cell cell-empty' : 'cell' } 
+						 data-id={ this.id }
+						 onClick={   (this.state.selected) ? () => this.setState({focused: true}) 
+										 			  									 : () => { this.setState({selected: true}); 
+																											   this.props.alert_selected(this.id) }}> 
 						<ReactMarkdown>{this.state.data}</ReactMarkdown> 
 				</div>
 			)
