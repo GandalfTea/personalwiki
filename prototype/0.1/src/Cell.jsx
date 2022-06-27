5d594648d754b9eed1bb5bd3ba5f6f9ccee9569c
Octavian Rusu <octavian.concept@gmail.com>
2022-06-27 17:13:37 +0100

    Cell maintains new-line correctly when edited

--- prototype/0.1/src/Cell.jsx
+++ prototype/0.1/src/Cell.jsx
@@ -27,31 +27,31 @@ class Cell extends React.Component {
 			// Cell is focused.
 			// On blur, copy the text to state to render
 			return(
-				<div className={ (this.state.data=="") ? 'cell cell-selected cell-empty' : 'cell cell-selected'} 
+				<div className={ (this.state.data=="") ? 'cell cell-input cell-selected cell-empty' : 'cell cell-input cell-selected'} 
 						 contentEditable tabIndex='0' ref={this.cell_text} 
 						 data-id={ this.id }
 						 onBlur={ () => {
-										this.setState({focused: false})
+								this.setState({focused: false})
 
-										// TODO: This does not store empty lines.
-										this.setState({ data: this.cell_text.current.innerText});
-										this._data = JSON.stringify(this.cell_text.current.innerText);
-										this.cell_text.current.innerText = "";
+								// TODO: This does not store empty lines.
+								this.setState({ data: this.cell_text.current.innerText.replaceAll('\n', '\n\n')});
+								//this._data = JSON.stringify(this.cell_text.current.innerText);
+								this.cell_text.current.innerText = "";
 
-										// Update ledger on File parent class
-										this.props.update_callback(this.id, this._data);
+								// Update ledger on File parent class
+								this.props.update_callback(this.id, this._data);
 							}}>
-				{this.state.data}</div>
+				{this.state.data.replaceAll('\n\n', '\r\n')}</div>
 			)
 		} else {
 			return(
 				<div className={ (this.props.selected !== "") ? (this.state.data=="") ? 'cell ' + this.props.selected + ' cell-empty'
-																																			 : 'cell ' + this.props.selected 
-																							 : (this.state.data=="") ? 'cell cell-empty' : 'cell' } 
+                                                                              : 'cell ' + this.props.selected 
+                                                      : (this.state.data=="") ? 'cell cell-empty' : 'cell' } 
 						 data-id={ this.id }
 						 onClick={   (this.props.selected !== "") ? () => this.setState({focused: true}) 
-										 			  									 : () => { this.setState({selected: true}); 
-																											   this.props.alert_selected(this.id) }}> 
+										 			  									 			  : () => { this.setState({selected: true}); 
+																											          this.props.alert_selected(this.id) }}> 
 						<ReactMarkdown>{this.state.data}</ReactMarkdown> 
 				</div>
 			)
