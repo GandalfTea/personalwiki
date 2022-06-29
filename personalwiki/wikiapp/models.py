f8ab88cb7728358e4046a25e85f8c3162b01f323
Octavian Rusu <octavian.concept@gmail.com>
2022-06-29 14:18:14 +0100

    Trying to link static assets to template HTML

--- personalwiki/wikiapp/models.py
+++ personalwiki/wikiapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+""" I am not sure if the cells should be stored separately
+    or simply in a single data field. Max theoretical file
+    size could get pretty big.
+
+    TODO: Research the CASCADE  """
+
+class Cell(models.Model):
+    data = models.CharField(max_length=5000)
+    idx = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.idx
+
+class File(models.Model):
+    name = models.CharField(max_length=200)
+    last_edit = models.DateField()
+    cells = models.ForeignKey(Cell, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Notebook(models.Model):
+    title = models.CharField(max_length=120)
+    files = models.ForeignKey(File, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
