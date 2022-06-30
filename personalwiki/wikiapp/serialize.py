5a5e3fbd016394d6fd445d2dacf3f9a1be3b8d33
Octavian Rusu <octavian.concept@gmail.com>
2022-06-30 18:09:26 +0100

    Update Model in README, update serializers for new model

--- personalwiki/wikiapp/serialize.py
+++ personalwiki/wikiapp/serialize.py
@@ -5,28 +5,25 @@ from wikiapp.models import Cell, File, Notebook
 
 # Serialize the data before sending it between the database and UI
 
-class CellSerializer(serializers.Serializer):
-    data = serializers.CharField(style={'base_template' : 'textarea.html'})
-    idx = serializers.IntegerField()
+class NotebookSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=120)
 
     def create(self, validated_data):
-        return Cell.objects.create(**validated_data)
+        return Notebook.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.data = validated_data.get('data', instance.data)
-        instance.idx = validated_data.get('idx', instance.idx)
+        instance.title = validated_data.get('title', instance.title)
         instance.save()
         return instance
 
     class Meta:
-        model = Cell
+        model = Notebook
 
 
 class FileSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=200)
     last_edit = serializers.DateTimeField()
-    #cells = serializers.RelatedField( source='cell') 
-    cells = CellSerializer(many=True) 
+    notebook = NotebookSerializer()
 
     def create(self, validated_data):
         return File.objects.create(**validated_data)
@@ -34,7 +31,7 @@ class FileSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.last_edit = validated_data.get('last_edit', instance.last_edit)
-        instance.cells = validated_data.get('cells', instance.cells)
+        instance.notebook = validated_data.get('notebook', instance.notebook)
         instance.save()
         return instance
 
@@ -42,21 +39,21 @@ class FileSerializer(serializers.Serializer):
         model = File
 
 
-class NotebookSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=120)
-    #files = serializers.RelatedField( source='file' )
-    files = FileSerializer(many=True) 
+class CellSerializer(serializers.Serializer):
+    data = serializers.CharField(style={'base_template' : 'textarea.html'})
+    idx = serializers.IntegerField()
+    main_file = FileSerializer()
 
     def create(self, validated_data):
-        return Notebook.objects.create(**validated_data)
+        return Cell.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.files = validated_data.get('files', instance.fiels)
+        instance.data = validated_data.get('data', instance.data)
+        instance.idx = validated_data.get('idx', instance.idx)
+        instance.main_file = validated_data.get('main_file', instance.main_file)
         instance.save()
         return instance
 
     class Meta:
-        model = Notebook
-
+        model = Cell
 
