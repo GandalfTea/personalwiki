235a22c6a7d6379f4a55c4ca10879f7248f6300e
Octavian Rusu <octavian.concept@gmail.com>
2022-07-11 12:46:31 +0100

    Get Cell instance by UUID

--- personalwiki/wikiapp/views.py
+++ personalwiki/wikiapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from wikiapp.serializers import CellSerializer, FileSerializer, NotebookSerializer
 from wikiapp.models import Cell, File, Notebook
@@ -8,6 +11,7 @@ from wikiapp.models import Cell, File, Notebook
 class CellViewSet(viewsets.ModelViewSet):
     serializer_class = CellSerializer
     queryset = Cell.objects.all()
+    lookup_field = 'uuid'
 
 class FileViewSet(viewsets.ModelViewSet):
     queryset = File.objects.all()
