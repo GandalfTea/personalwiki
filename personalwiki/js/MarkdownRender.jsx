f315eb1dadbdc620c697e66301232dd59d5f199c
Octavian Rusu <octavian.concept@gmail.com>
2022-08-03 11:09:52 +0300

    Change selection border to outline, make MarkdownRender a Class and do not re-render when selecting

--- personalwiki/js/MarkdownRender.jsx
+++ personalwiki/js/MarkdownRender.jsx
@@ -1,5 +1,5 @@
 
-//import React from 'react';
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import MathJax from 'react-mathjax';
 import remarkMath from 'remark-math';
@@ -12,24 +12,37 @@ import remarkMath from 'remark-math';
 	and <inlineMath> HTML objects.
 */
 
-function MarkdownRender(props) {
-	const newProps = {
-		...props,
-		remarkPlugins: [
-			remarkMath,
-		],
-		components: {
-			div: (props) =>
-				<MathJax.Node formula={props.children} />,
-			span: (props) =>
-				<MathJax.Node inline formula={props.children} />
+class MarkdownRender extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.newProps = {
+			...this.props,
+			remarkPlugins: [
+				remarkMath,
+			],
+			components: {
+				div: (props) =>
+					<MathJax.Node formula={props.children} />,
+				span: (props) =>
+					<MathJax.Node inline formula={props.children} />
+			}
+		};
+	}
+
+	shouldComponentUpdate(nextProps) {
+		if( nextProps == this.props ) {
+			return false;
 		}
-	};
-	return(
-		<MathJax.Provider input="tex">
-			<ReactMarkdown  {...newProps}></ReactMarkdown>
-		</MathJax.Provider>
-	);
+	}
+
+	render() {
+		return(
+			<MathJax.Provider input="tex">
+				<ReactMarkdown {...this.newProps}></ReactMarkdown>
+			</MathJax.Provider>
+		);
+	}
 }
 
 export default MarkdownRender;
