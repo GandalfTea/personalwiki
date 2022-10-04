0f9c15e63bb1630a5f083f7b658101107635f6cd
Octavian Rusu <octavian.concept@gmail.com>
2022-10-04 13:55:33 -0600

    Fetch and construct UI for dir tree

--- personalwiki/js/tree.tsx
+++ personalwiki/js/tree.tsx
@@ -23,40 +23,54 @@ const FileEntry = function(props) {
 }
 
 
-var notebooks = [];
+
+/*	Get Data From Server and Construct UI */
+
+function fetch_files_of_notebook( notebook: string ) {
+	return new Promise(function (resolve, reject) {
+		//console.log(notebook);
+		var payload = { "name": notebook };
+		var req = new XMLHttpRequest();
+		req.open("POST", "http://localhost:8000/api/notebook/files", true);
+		req.setRequestHeader("Content-Type", 'application/json');
+		req.send(JSON.stringify(payload));
+		req.onreadystatechange = () => {
+			if( req.readyState == 4 && req.status == 200) {
+				console.log(req.responseText);
+				resolve(req.responseText);
+			}
+		}
+	});
+}
 
 
 
-/*	Get Data From Server */
+(async () => {
 
-async function fetch_notebooks() {
-	fetch('/api/notebooks/').then(res => res.json()).then( (result) => {
+	var UI = [];
+
+ 	await fetch('/api/notebooks/').then(res => res.json()).then( async (result) => {
 		for( let i = 0; i < result.length; i++) {
-			console.log(result[i]['title']);
+			let files = await fetch_files_of_notebook(result[i]['title']);
+			let files_ui = [];
+			for( let j of JSON.parse(files) ) {
+				files_ui.push( <FileEntry name={j['name']} />);	
+			}
+			UI.push( <NotebookEntry name={result[i]['title']} files={files_ui} />);
 		}
 	}
-}
 
-async function fetch_files_of_notebook( notebook: string) {
-	var payload = { "name": notebook };
-	var req = new XMLHttpRequest();
-	req.open("POST", "http://localhost:8000/api/notebook/files");
-	req.setRequestHeader("Content-Type", 'application/json');
-	req.send(JSON.stringify(payload));
-	req.onreadystatechange = () => {
-		if( req.readyState == 4 && req.status == 200) {
-			console.log(req.responseText);
-		}
-	}
-}
 
-async function fetch_tree() {
-	const notebooks = fetch_notebooks();
-	for( i of notebooks) {
-		fetch_files_of_notebook(i);
-	}
-}
+	const root = ReactDOM.createRoot(
+		document.getElementById('root')
+	);
 
-fetch_tree();
+	root.render(
+		<div>
+			{UI}
+		</div>
+	);
+
+})()
 
 
