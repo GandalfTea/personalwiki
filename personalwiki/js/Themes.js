1484278206c16d6bfa2055293f85a970479b1ac5
Octavian Rusu <octavian.concept@gmail.com>
2022-10-10 00:57:08 -0600

    implementing customisable themes

---
+++ personalwiki/js/Themes.js
@@ -0,0 +1,29 @@
+
+
+export const Light = {
+	background: '#fff',
+	text: '#333',
+	header_address: '#888',
+	header_title: '#000',
+	cell_idle: '#fafafa',
+	cell_idle_border: '#ddd',
+	cell_selected: '#444',
+	cell_selected: '#fafafa',
+	cell_selected_border: '#42A5F5',
+	cell_editing: '#fff',
+	cell_editing_border: '#66BB6A',
+}
+
+export const Dark = {
+	background: '#121212',
+	text: '#aaa',
+	header_address: '#888',
+	header_title: '#FFF',
+	cell_idle: '#444',
+	cell_idle_border: '#222',
+	cell_selected: '#444',
+	cell_selected_border: '#42A5F5',
+	cell_editing: '#121212',
+	cell_editing_border: '#66BB6A',
+}
+
