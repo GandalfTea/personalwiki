2f0ec9e9cb182efb6eb0c778b44c9c4085bbcb04
GandalfTea <octavian.concept@gmail.com>
2023-03-21 12:46:00 -0600

    rm django overhead

--- start.sh
+++
@@ -1,8 +0,0 @@
-
-if python -c "import django, rest_framework" &> /dev/null; then
-	echo ' Python requirements already satisfied.'
-else
-	pip install -r ./requirements.txt
-fi
-
-cd personalwiki && python manage.py runserver
--- usertest/README.md
+++
@@ -1,4 +0,0 @@
-
-#### Analytical User Data Mining for Software Improvements
-
-Do not collect any personal data. Don't be a dick.
