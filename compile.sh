413af61755f27cda64a10a272d4b1690473d440b
GandalfTea <octavian.concept@gmail.com>
2023-06-01 17:00:11 -0600

    write to file test fails

---
+++ compile.sh
@@ -0,0 +1,2 @@
+tsc ./server/app.ts --outDir ./public/compiled/
+node ./public/compiled/app.js
--- tests/test_todisk.py
+++ tests/test_todisk.py
@@ -7,17 +7,20 @@ class TestFlush(unittest.TestCase):
 	
 	def test_file_creation(self):
 		data = {
-			"0" : {
-				"uuid" : "da39a3ee5e6b4b0d3255bfef95601890afd80709"
-				"data" : "demo data for a demo world"
+			"cells": {
+							"0" : {
+								"uuid" : "da39a3ee5e6b4b0d3255bfef95601890afd80709",
+								"data" : "demo data for a demo world"
+							},
+							"1" : {
+								"uuid" : "85136c79cbf9fe36bb9d05d0639c70c265c18d37",
+								"data" : "demo data for a demo world"
+							},
+							"2": {
+								"uuid" : "e49524050d4b8e04f4d0e886b82921d74e58f051",
+								"data" : "demo data for a demo world"
+							}
 			},
-			"1" : {
-				"uuid" : "85136c79cbf9fe36bb9d05d0639c70c265c18d37"
-				"data" : "demo data for a demo world"
-			},
-			"2": {
-				"uuid" : "e49524050d4b8e04f4d0e886b82921d74e58f051"
-				"data" : "demo data for a demo world"
-			}
+			"nb": "demonb"
 		}
-
+		r = rq.post(f"localhost:{PORT}/file/demofile", headers={"Content-Type": "application/json"}, data=json.dumps(data))
