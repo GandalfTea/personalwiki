1e0fc52ef2e004ba3cc6ba1d233621abf2e6fa06
GandalfTea <octavian.concept@gmail.com>
2023-06-03 14:03:22 -0600

    error checking and html responses

--- server/routes/api.routes.ts
+++ server/routes/api.routes.ts
@@ -1,5 +1,5 @@
 
-import {diskret, write_file} from "../fs";
+import {diskret, write_file, write_file_safe} from "../fs";
 import log from "../logging";
 const express = require("express");
 const router = express.Router();
@@ -24,28 +24,34 @@ router.get("/file/:slug", (req, res) => {
 
 // Post new full array of cells (this happens often because of cell movement) 
 router.post("/file/:slug", (req, res) => {
-	console.log(process.env.DEBUG)
 	const _start = process.hrtime.bigint();
 	if(process.env.DEBUG >= 2) console.log(req.body);
-	let ret = write_file(req.body.data, req.params.slug, req.body.nb);	
-	console.log(typeof _start)
+	let ret = write_file_safe(req.body.data, req.params.slug, req.body.nb);	
 	switch(ret) {
 		case diskret.SUCCESS: 
-			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 200, "SUCCESS");
-			res.status(200);
-			res.send("Done")	
+			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 201, "SUCCESS. File "+req.params.slug+ " created.");
+			res.status(201);
+			res.send("SUCCESS");	
 			break;
 		case diskret.INVALID_ARGUMENTS:
-			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 200, "INVALID_ARGUMENTS");
+			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 400, "INVALID_ARGUMENTS");
+			res.status(400);
+			res.send("INVALID_ARGUMENTS");	
 			break;
 		case diskret.PARENT_NOTEBOOK_NOT_FOUND:
-			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 200, "PARENT_NOTEBOOK_NOT_FOUND");
+			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 400, "PARENT_NOTEBOOK_NOT_FOUND");
+			res.status(400);
+			res.send("PARENT_NOTEBOOK_NOT_FOUND"); 
 			break;
 		case diskret.OVERWRITE_FROM_SAFE_FUNCTION:
-			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 200, "OVERWRITE_FROM_SAFE_FUNCTION");
+			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 403, "OVERWRITE_FROM_SAFE_FUNCTION");
+			res.status(403);
+			res.send("OVERWRITE_FROM_SAFE_FUNCTION");	
 			break;
 		case diskret.FS_ERROR:
-			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 200, "FS_ERROR");
+			log("POST", req.socket.remoteAddress, Number(process.hrtime.bigint() - _start), 500, "FS_ERROR");
+			res.status(500);
+			res.send("FS_ERROR");	
 			break;
 	}
 });
