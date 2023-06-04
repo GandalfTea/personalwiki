f7d746d1155f4494ddee43729333753e2f2ebae4
GandalfTea <octavian.concept@gmail.com>
2023-06-04 00:32:18 -0600

    sh install

---
+++ install.sh
@@ -0,0 +1,4 @@
+
+npm install .
+tsc ./server/app.ts --outDir ./public/compiled/
+node ./public/compiled/app.js
--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-npm install .
-tsc ./server/app.ts --outDir ./public/compiled/
 node ./public/compiled/app.js
